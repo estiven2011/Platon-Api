eba93459496e80c329d9bb4dfc2da00802e4c963
estiven2011 <juan_posada23221@elpoli.edu.co>
2025-04-09 19:26:05 -0500

    implementacion service,en anterior commit implemente el dao

---
+++ src/main/java/co/edu/poli/ces3/platonapi/service/CursoService.java
@@ -0,0 +1,40 @@
+package co.edu.poli.ces3.platonapi.service;
+
+import co.edu.poli.ces3.platonapi.dao.Curso;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CursoService {
+    private static List<Curso> cursos = new ArrayList<>();
+
+    public boolean agregarCurso(Curso nuevo) {
+        // validacion curso diferente nombre
+        for (Curso c : cursos) {
+            if (c.getNombre().equalsIgnoreCase(nuevo.getNombre())) {
+                return false;
+            }
+        }
+
+        // validacion cupo maximo mayor a 0
+        if (nuevo.getCupoMaximo() <= 0) {
+            return false;
+        }
+
+        cursos.add(nuevo);
+        return true;
+    }
+
+    public List<Curso> listarTodos() {
+        return cursos;
+    }
+
+    public List<Curso> buscarCursosPorFacultad(String facultad) {
+        List<Curso> resultado = new ArrayList<>();
+        for (Curso c : cursos) {
+            if (c.getFacultad().equalsIgnoreCase(facultad)) {
+                resultado.add(c);
+            }
+        }
+        return resultado;
+    }
+}
---
+++ src/main/java/co/edu/poli/ces3/platonapi/servlet/CursoServlet.java
@@ -0,0 +1,4 @@
+package co.edu.poli.ces3.platonapi.servlet;
+
+public class CursoServlet {
+}
